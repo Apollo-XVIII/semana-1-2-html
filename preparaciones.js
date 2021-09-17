24e20fb6db66ee312bc059a3df749bb174937b0a
alexangupe <alexangup@gmail.com>
2021-09-17 16:32:11 -0500

    Ajuste llamado a la preparacion

--- preparaciones.js
+++ preparaciones.js
@@ -7,16 +7,16 @@ Descripción: función para el llamado de la preparacion desde la sección de pr
 
 let verPreparacion = () => {
     //Traer todos los botones de la seccion
-    const recipeBtns = document.querySelectorAll(".btn-warning");
+    const mixBtns = document.querySelectorAll(".btn-warning");
     
     //Obtener el botón que dió clic
     const clicBtnRecipe = function (evento) {
         console.log = "El botón fue..."
     }
     // botones es un arreglo así que lo recorremos
-    recipeBtns.forEach(recipeBtn => {
+    mixBtns.forEach(mixBtns => {
         //Agregar listener
-        recipeBtn.addEventListener("click", clicBtnRecipe);
+        mixBtns.addEventListener("click", clicBtnRecipe);
         //Ir a la página de la preparacion
         window.location = "./preparacion.html"
     });
