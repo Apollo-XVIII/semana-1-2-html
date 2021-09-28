04e39579814d5d21bd5b971719d28236a0510e6b
MauricioRuizD <mauricioruizdiaz@gmail.com>
2021-09-27 23:05:45 -0500

    Pie ajustado

--- simply1/src/components/Pie.vue
+++ simply1/src/components/Pie.vue
@@ -2,15 +2,19 @@
 <v-footer dark padless>
     <v-card class="flex" flat tile>
         <v-spacer></v-spacer>
-        <v-card-title class="brown lighten-1  text-center ">
+        <v-card-title class="brown lighten-1">
             <br>
             <br>
-            <strong class=" brown lighten-1 lighten-5--text text-center">CONTACTO: <br>
-            </strong>
-            <strong class="darken-4--text"> <br>
+
+            <p class="  darken-5--text font-weight-light"> CONTACTO: <br> </p>
+
+            <v-spacer></v-spacer>
+            <br>
+            <h5 class=" font-weight-light">
 
                 Oficina principal: Carrera 123 # 45 - 67 Bogotá<br>
-                Tel:(+57) 3106185163</strong>
+                Tel:(+57) 3106185163
+            </h5>
 
             <v-spacer></v-spacer>
 
@@ -22,7 +26,7 @@
             <br>
         </v-card-title>
 
-        <v-card-text class=" brown darken-4 lighten-5--text text-justify-left">
+        <v-card-text class=" brown darken-4  brown--text text-justify-left">
 
             <strong>© Simply Coffee / una marca de Apollo XVIII inc — {{ new Date().getFullYear() }}<br>
                 © Todos los derechos reservados </strong>
