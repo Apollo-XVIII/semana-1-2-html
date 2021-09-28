32d782b1dcd51cddaf7cbc25c44b19a610925141
MauricioRuizD <mauricioruizdiaz@gmail.com>
2021-09-27 22:37:59 -0500

    Nosotros ajustada

--- simply1/src/views/Nosotros.vue
+++ simply1/src/views/Nosotros.vue
@@ -1,13 +1,22 @@
 <template>
-<v-container class="brown lighten-3 brown--text font-weight-light">
-    <h1 class="font-weight-light">
-        Nosotros
-    </h1>
+<v-container class="brown lighten-3 brown--text font-weight-light" >
+    <v-row  justify="space-around">
+        
+            <h1 class="font-weight-light">
+                Nosotros
+            </h1 >
+    </v-row>
+    <v-row  justify="space-around">
+        
+            <h3 class="font-weight-light">
+                Este es el equipo de trabajo del Proyecto Apollo XVIII !!!
+            </h3 >
+    </v-row>
     <template>
         <v-container class="brown lighten-3 brown--text">
             <v-row no-gutters>
                 <v-col>
-                    <v-card class="pa-3 ">
+                    <v-card class="pa-10" height="500px">
                         <template>
 
                             <v-row justify="space-around">
@@ -18,21 +27,26 @@
                             <v-row justify="space-around">
                                 <h4 class=" font-weight-medium  text-center">
                                     Monica A. Angulo P.
+
                                 </h4>
                             </v-row>
                             <v-row justify="space-around">
                                 <h5 class="font-weight-light text-center">
-                                    Ingeniera de Sistemas del Politécnico Grancolombiano,<br>
+                                    <br>Ingeniera de Sistemas del Politécnico Grancolombiano,<br>
                                     con experiencia en desarrollo de software, <br> Gerencia de proyectos y <br>
                                     conocimiento y aplicación de metodologías, <br> modelos y normas de calidad <br>
                                     enfocadas en el área TI. <br>(SCRUM, CMMI, ITMARK, ITIL, ISO27000, ISO20000).
+                                </h5> <br>
+                                <h5 class="font-weight-medium text-center font-italic">
+                                    <br>
+                                    "Amigos y café, la mejor combinación"
                                 </h5>
                             </v-row>
                         </template>
                     </v-card>
                 </v-col>
                 <v-col>
-                    <v-card class="pa-3 ">
+                    <v-card class="pa-10 " height="500px">
                         <template>
 
                             <v-row justify="space-around">
@@ -47,17 +61,22 @@
                             </v-row>
                             <v-row justify="space-around">
                                 <h5 class="font-weight-light text-center">
-                                    Ingeniera de Sistemas del Politécnico Grancolombiano,<br>
-                                    con experiencia en desarrollo de software, <br> Gerencia de proyectos y <br>
-                                    conocimiento y aplicación de metodologías, <br> modelos y normas de calidad <br>
-                                    enfocadas en el área TI. <br>(SCRUM, CMMI, ITMARK, ITIL, ISO27000, ISO20000).
+                                    <br>Técnico en Sistemas y Computación <br>
+                                    Desarrollador de software durante <br>
+                                    más de veinte años, <br>,
+                                    astrónomo aficionado, fotógrafo <br>
+                                    y amante de la ciencia ficción. <br><br><br>
+                                </h5>
+                                <h5 class="font-weight-medium text-center font-italic">
+                                    <br>
+                                    "En cada sorbo de café hay una buena idea escondida"
                                 </h5>
                             </v-row>
                         </template>
                     </v-card>
                 </v-col>
                 <v-col>
-                    <v-card class="pa-3 ">
+                    <v-card class="pa-10 " height="500px">
                         <template>
 
                             <v-row justify="space-around">
@@ -72,17 +91,23 @@
                             </v-row>
                             <v-row justify="space-around">
                                 <h5 class="font-weight-light text-center">
-                                    Ingeniera de Sistemas del Politécnico Grancolombiano,<br>
-                                    con experiencia en desarrollo de software, <br> Gerencia de proyectos y <br>
-                                    conocimiento y aplicación de metodologías, <br> modelos y normas de calidad <br>
-                                    enfocadas en el área TI. <br>(SCRUM, CMMI, ITMARK, ITIL, ISO27000, ISO20000).
+                                    <br>Comunicador Social - Periodista de la <br>
+                                    Universidad de la Sabana. <br>
+                                    Amplia experiencia en investigación de mercados, <br>
+                                    análisis de datos, técnicas de recolección <br>
+                                    de información y diseño de estrategias de <br>
+                                    planeación estratégica.
+                                </h5>
+                                <h5 class="font-weight-medium text-center font-italic">
+                                    <br>
+                                    "La felicidad es un buen libro y una taza de café"
                                 </h5>
                             </v-row>
                         </template>
                     </v-card>
                 </v-col>
                 <v-col>
-                    <v-card class="pa-3 ">
+                    <v-card class="pa-10" height="500px">
                         <template>
 
                             <v-row justify="space-around">
@@ -97,10 +122,14 @@
                             </v-row>
                             <v-row justify="space-around">
                                 <h5 class="font-weight-light text-center">
-                                    Ingeniera de Sistemas del Politécnico Grancolombiano,<br>
-                                    con experiencia en desarrollo de software, <br> Gerencia de proyectos y <br>
-                                    conocimiento y aplicación de metodologías, <br> modelos y normas de calidad <br>
-                                    enfocadas en el área TI. <br>(SCRUM, CMMI, ITMARK, ITIL, ISO27000, ISO20000).
+                                    <br>Ingeniero Electronico - Universidad <br>
+                                    Santo Tomas - Experiencia en desarrollo <br>
+                                    de maquinaria CNC, Impresoras 3D, <br>
+                                    músico aficionado.<br><br><br>
+                                </h5>
+                                <h5 class="font-weight-medium text-center font-italic">
+                                    <br>
+                                    "Cafe, el origen y combustible de grandes proyectos"
                                 </h5>
                             </v-row>
                         </template>
